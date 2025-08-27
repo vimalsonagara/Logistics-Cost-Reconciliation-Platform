02bb9541ffd2d0915a90496471159bac2f56f7a5
vimalsonagara <97290857+vimalsonagara@users.noreply.github.com>
2025-08-27 18:59:48 +0530

    Create logger.py

---
+++ backend/utils/logger.py
@@ -0,0 +1,8 @@
+import logging, sys
+
+logger = logging.getLogger("logistics")
+logger.setLevel(logging.INFO)
+handler = logging.StreamHandler(sys.stdout)
+formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
